f661be80a49e51217587ed5410d9c5aedf87af7c
danishakh <danishakh@gmail.com>
2019-05-30 02:17:36 -0400

    added AppBar component with a link to homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { Grid } from '@material-ui/core';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Darkness My Old Friend</h1>
-    </div>
+    <Grid style={{height: '100%'}}>
+        <Router>
+          <div className="container">
+            <Navbar />
+          </div>
+
+          {/* <Switch>
+            <Route path="/" exact component={TodoList} />
+            <Route path="/edit/:id" component={EditTodo} />
+            <Route path="/create" component={CreateTodo} />
+            <Route component={NotFound} />
+          </Switch> */}
+
+        </Router>
+      </Grid>
   );
 }
 
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Link as BrowserLink } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Link from '@material-ui/core/Link';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CodeIcon from '@material-ui/icons/Code';
+
+
+export default class Navbar extends Component {
+    
+
+    render() {
+
+        return (
+            <AppBar position="static" color="default">
+                <Toolbar>
+                    <Typography variant="h5">
+                        <Link
+                            style={{fontFamily: "monospace"}}
+                            color='textPrimary'
+                            underline='none'
+                            component={BrowserLink}
+                            to='/'
+                        >
+                            <CodeIcon /> MERN Login App - with JSON Web Tokens
+                        </Link>
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        );
+    }
+}
