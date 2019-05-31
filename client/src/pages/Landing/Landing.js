78251e7c6bce6d146412c6dd9cd34255d1c738da
danishakh <danishakh@gmail.com>
2019-05-31 02:21:03 -0400

    Landing Page layout complete for now

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -11,21 +11,22 @@ export default class Landing extends Component {
         return(
 
             <Container maxWidth="md" style={{marginTop: 100}}>
-                <Grid container justify='center' alignItems="center" direction="column" spacing={6}>
+                <Grid container justify='space-between' alignItems="center" direction="column" spacing={6}>
                     <Grid item>
                         <Typography align="center" color="textPrimary" variant="h4">
                                 Simple Login/Auth App with the <span style={{fontFamily: 'monospace'}}>MERN</span> stack from scratch
                         </Typography>
+                    </Grid>
+                    <Grid item>
                         <br />
                         <br />
-                        <Typography align="center" color="textPrimary" variant="h5">
-                            Create a minimal full-stack app with user authentication via Passport and JWTs
+                        <Typography align="center" color="textPrimary" variant="h6">
+                            This is a minimal full-stack app to showcase user authentication via Passport and JWTs
                         </Typography>
                     </Grid>
                     
-                    <br />
-                    <Grid container direction="row"  spacing={6}>
-                        <Grid item xs={6}>
+                    <Grid container justify="center" alignItems="center" direction="row"  spacing={6} style={{marginTop: 50}}>
+                        <Grid item sm={6} md={3}>
                             <Button
                                 style={{width: 200, height:80, borderRadius: 10, letterSpacing: 1.5}}
                                 variant="contained" 
@@ -38,7 +39,7 @@ export default class Landing extends Component {
                                 Register
                             </Button>
                         </Grid>
-                        <Grid item xs={6}>
+                        <Grid item sm={6} md={3}>
                             <Button 
                                 style={{width: 200, height:80, borderRadius: 10, letterSpacing: 1.5}}
                                 variant="contained" 
